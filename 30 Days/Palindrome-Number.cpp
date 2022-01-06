f77de3568b29eead834f2d6a99338dfb307be2d3
venki04 <61339956+venki04@users.noreply.github.com>
2022-01-07 00:59:12 +0530

    Create Palindrome-Number.cpp
    #Day1
    Palindrome Number - Brute force solution

---
+++ 30 Days/Palindrome-Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int temp,s=x;
+        long int y=0;
+        
+        if(x<0)
+            { 
+            return false;
+            }
+        
+        else{
+            while(s)
+                {temp=s%10;
+                y=y*10+temp;
+                s=s/10;
+                }
+            if(y==x)
+                return true;
+        return false;   
+            }
+                            }       
+
+};
+# https://leetcode.com/problems/palindrome-number/
